44775b62bafa62c763a9bf9ff1f2337fceac91ab
ViktorRybakov <viktor.rybakov@gmail.com>
2020-02-18 10:15:15 +0300

    Изменил билд

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -125,9 +125,38 @@ let accordionTab3 = document.querySelector('.js-tabs__accordion_tab3');
 if ( document.documentElement.clientWidth <= 768 ) {
   visuallyHidden.classList.remove('visually-hidden');
   container1.classList.remove('tabs__container_active');
+  container1.setAttribute('aria-expanded', true);
   container2.classList.remove('tabs__container_active');
+  container2.setAttribute('aria-expanded', true);
   container3.classList.remove('tabs__container_active');
+  container3.setAttribute('aria-expanded', true);
 }
+
+window.addEventListener('resize', function(event) {
+  if ( document.documentElement.clientWidth > 768 ) {
+    visuallyHidden.classList.add('visually-hidden');
+    container1.classList.add('tabs__container_active');
+    container1.setAttribute('aria-expanded', true);
+    tabsContent1.setAttribute('aria-expanded', true);
+    container2.classList.remove('tabs__container_active');
+    container2.setAttribute('aria-expanded', false);
+    tabsContent2.setAttribute('aria-expanded', false);
+    container3.classList.remove('tabs__container_active');
+    container3.setAttribute('aria-expanded', false);
+    tabsContent3.setAttribute('aria-expanded', false);
+  } else if ( document.documentElement.clientWidth <= 768 ) {
+    visuallyHidden.classList.remove('visually-hidden');
+    container1.classList.remove('tabs__container_active');
+    container1.setAttribute('aria-expanded', true);
+    tabsContent1.setAttribute('aria-expanded', false);
+    container2.classList.remove('tabs__container_active');
+    container2.setAttribute('aria-expanded', true);
+    tabsContent2.setAttribute('aria-expanded', false);
+    container3.classList.remove('tabs__container_active');
+    container3.setAttribute('aria-expanded', true);
+    tabsContent3.setAttribute('aria-expanded', false);
+  }
+});
 
 accordionTab1.onclick = function() {
   accordionTab1.classList.toggle('tabs__accordion_active');
